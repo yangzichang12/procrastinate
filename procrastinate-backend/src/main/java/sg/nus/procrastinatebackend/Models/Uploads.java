6c0c578095136b1f827101a56af4682c3c52faa0
Alejandro Hou Lu <alejandro.hlcw@gmail.com>
2024-03-24 23:50:46 +0800

    Upload feature done, data persistence done

--- procrastinate-backend/src/main/java/sg/nus/procrastinatebackend/Models/Uploads.java
+++ procrastinate-backend/src/main/java/sg/nus/procrastinatebackend/Models/Uploads.java
@@ -1,11 +1,16 @@
 package sg.nus.procrastinatebackend.Models;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
 public class Uploads {
 
     private String uploadId;
     private String username;
     private String contentUrl;
     private String contentType;
+    private String resultUrl;
+    private String result;
 
     public String getUploadId() {
         return uploadId;
@@ -31,11 +36,34 @@ public class Uploads {
     public void setUsername(String username) {
         this.username = username;
     }
+    public String getResultUrl() {
+        return resultUrl;
+    }
+    public void setResultUrl(String resultUrl) {
+        this.resultUrl = resultUrl;
+    }
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+
     @Override
     public String toString() {
         return "Uploads [uploadId=" + uploadId + ", username=" + username + ", contentUrl=" + contentUrl
-                + ", contentType=" + contentType + "]";
+                + ", contentType=" + contentType + ", resultUrl=" + resultUrl + "]";
     }
+
+    public JsonObject toJson(){
+        return Json.createObjectBuilder()
+            .add("uploadId",uploadId)
+            .add("username",username)
+            .add("contentUrl",contentUrl)
+            .add("contentType",contentType)
+            .build();
+    }
+
 
 
     
--- procrastinate_data_processor/procrastinate/views.py
+++ procrastinate_data_processor/procrastinate/views.py
@@ -1,12 +1,12 @@
-from types import GeneratorType
-from django.http import response
+import json
+import mimetypes
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from nbformat.v3 import nbformat
-from nbconvert import HTMLExporter
-from .ml_model import execute_speech_to_text_model
-from .models import get_user
-from django.views.decorators.http import require_GET, require_http_methods
+from . import ml_model
+from .models import Uploads, get_user
+from . import services
+from django.views.decorators.http import require_GET, require_POST
+import boto3
 
 user = get_user()
 
@@ -19,14 +19,57 @@ def home(request):
 
 @csrf_exempt
 def pong(request):
-    if request.method == 'GET':
+    if request.method == 'GET':      
         return HttpResponse('pong')
     else:
         return HttpResponse('invalid request',status=400)
 
 @csrf_exempt
-@require_GET
+@require_POST
 def speechToText(request):
-    model_output = execute_speech_to_text_model()
+
+    print('hello')
+    print(request.body)
+
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+    except Exception as ex:
+        print('error in json load', ex)
+
+    username = data.get('username')
+    uploadId = data.get('uploadId')
+    contentUrl = data.get('contentUrl')
+    contentType = data.get('contentType')
+
+    upload = Uploads()
+    upload.username = username
+    upload.upload_id = uploadId
+    upload.content_url = contentUrl
+    upload.content_type = contentType
+
+    print(upload)
+
+    # download the file from s3
+    # save to static/audio folder
+    # return the file path for ml model to pick up the file 
+    file_path = services.download_and_save_file(upload)
+
+    # result map contains the output path - static/output and the transcribed text
+    result_map = ml_model.execute_speech_to_text_model(file_path, uploadId)
+
+    # Upload result file to s3 bucket and get its url
+    result_url = services.upload_result(result_map['output_path'], uploadId, username)
+    print('Result url: ' + result_url)
+
+    # Persist the result url in the db
+    services.update_db_uploads_resultUrl(uploadId,result_url)
+
+    response_data_map = {
+        'result_url': result_url,
+        'result': result_map['transcribed_text'],
+        'username': username
+    }
+    response_data_json = json.dumps(response_data_map)
     
-    return HttpResponse(model_output)
+    return HttpResponse(response_data_json, content_type='application/json',
+                        status=200)
