6af2c3700157ce73dfc65a5dd82e3174a9d85b09
Jane Lee <janejessica2312@gmail.com>
2024-03-26 01:39:44 +0800

    updated tscofig from js to ts

--- procrastinate-frontend/app/page.tsx
+++ procrastinate-frontend/app/page.tsx
@@ -2,7 +2,7 @@
 import theme from './Theme/theme'; 
 import { ThemeProvider } from 'styled-components';
 import Navbar from "./Navbar/navBarContainer";
-import Footer from "./Footer/footer";
+import Footer from "./Footer/Footer"
 import BodyContainer from "./Body/bodyContainer";
 
 export default async function Home() {
